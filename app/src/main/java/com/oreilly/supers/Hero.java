eabaeabf7e222a30eda16e5957685037d52fb401
Kenneth Kousen <ken.kousen@kousenit.com>
2015-06-10 03:35:00 -0700

    Initial commit

---
+++ app/src/main/java/com/oreilly/supers/Hero.java
@@ -0,0 +1,38 @@
+package com.oreilly.supers;
+
+public class Hero {
+    private String name;
+    private String first;
+    private String last;
+    private int image;
+
+    public static final Hero[] heroes = {
+            new Hero("Mr. Incredible", "Robert", "Parr", R.drawable.mr_incredible),
+            new Hero("ElastiGirl", "Helen", "Parr", R.drawable.elastigirl),
+            new Hero("Frozone", "Lucius", "Best", R.drawable.frozone),
+            new Hero("Gazerbeam", "Simon", "Paladino", R.drawable.gazerbeam)
+    };
+
+    public Hero(String name, String first, String last, int image) {
+        this.name = name;
+        this.first = first;
+        this.last = last;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
